2643a2675906272fc9ad2fbf04c8572a7fe6fbe6
sunguh0904 <sunguh0904@gmail.com>
2024-12-26 13:22:38 +0900

    create chap02 section02 preparedstatement Application

---
+++ JDBC/chap02/src/section02/preparedstatement/Application3.java
@@ -0,0 +1,62 @@
+package section02.preparedstatement;
+
+import com.ohgiraffers.model.dto.EmployeeDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.getConnection;
+
+public class Application3 {
+    public static void main(String[] args) {
+
+        /* Scanner를 이용해서 사번을 입력받아 사원의 정보를 EmployeeDTO 객체에 담아서 출력 */
+        Connection con = getConnection();
+
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+
+        EmployeeDTO selectedEmp = null;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("조회하실 사번을 입력해주세요 : ");
+        String empId = sc.nextLine();
+
+        String query = "SELECT * FROM EMPLOYEE WHERE EMP_ID = ?";
+        try {
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, empId);
+
+            rset = pstmt.executeQuery();
+
+            if (rset.next()) {
+                selectedEmp = new EmployeeDTO();
+                selectedEmp.setEmpNO(rset.getString("EMP_ID"));
+                selectedEmp.setEmpNAME(rset.getString("EMP_NAME"));
+                selectedEmp.setEmpNO(rset.getString("EMP_NO"));
+                selectedEmp.setEmail(rset.getString("EMAIL"));
+                selectedEmp.setPhone(rset.getString("PHONE"));
+                selectedEmp.setDeptCODE(rset.getString("DEPT_CODE"));
+                selectedEmp.setJobCODE(rset.getString("JOB_CODE"));
+                selectedEmp.setSalLEVEL(rset.getString("SAL_LEVEL"));
+                selectedEmp.setSalary(rset.getInt("SALARY"));
+                selectedEmp.setBonus(rset.getDouble("BONUS"));
+                selectedEmp.setManagerID(rset.getString("MANAGER_ID"));
+                selectedEmp.setHireDATE(rset.getDate("HIRE_DATE"));
+                selectedEmp.setEntDATE(rset.getDate("ENT_DATE"));
+                selectedEmp.setManagerID(rset.getString("ENT_YN"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(rset);
+            close(pstmt);
+            close(con);
+        }
+        System.out.println("selectedEmp = " + selectedEmp);
+    }
+}
---
+++ JDBC/chap02/src/section02/preparedstatement/Application5.java
@@ -0,0 +1,91 @@
+package section02.preparedstatement;
+
+import model.EmployeeDTO;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+
+import static common.JDBCTemplate.close;
+import static common.JDBCTemplate.getConnection;
+
+public class Application5 {
+    public static void main(String[] args){
+
+        /* 조회할 사원의 성씨를 입력 받아 해당 성씨를 가진 사원 정보들을 모두 출력 */
+
+        Connection con = getConnection();
+
+        PreparedStatement pstmt = null;
+        // 불안전한 쿼리
+        ResultSet rset = null;
+
+        EmployeeDTO row = null;
+        List<EmployeeDTO> empList = null;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("조회할 이름의 성을 입력하세요 : ");
+        String empName = sc.nextLine();
+
+        Properties prop = new Properties();
+        // employee-query.xml 을 작성하여 쿼리 실행
+
+
+        try {
+            prop.loadFromXML(new FileInputStream("src/main/java/com/ohgiraffers/section02/preparedstatement/employee-query.xml"));
+            String query = prop.getProperty("selectEmpByFamilyName");
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, empName);
+            System.out.println("query = " + query);
+
+            rset = pstmt.executeQuery();
+            // executeQuery: 쿼리문으로 보내는 역할
+
+            empList = new ArrayList<>();
+            // 조회된 여러 행을 담아놓을 ArrayList 객체 생성
+
+            while (rset.next()) {
+                // 반복을 실행하며 반복이 실행될때마다 새로운 EmployeeDTO 객체를
+                // 생성해서새로운 값을 받을 준비를 하는 코드
+
+                row = new EmployeeDTO();
+                row.setEmpNO(rset.getString("EMP_ID"));
+                row.setEmpNAME(rset.getString("EMP_NAME"));
+                row.setEmpNO(rset.getString("EMP_NO"));
+                row.setEmail(rset.getString("EMAIL"));
+                row.setPhone(rset.getString("PHONE"));
+                row.setDeptCODE(rset.getString("DEPT_CODE"));
+                row.setJobCODE(rset.getString("JOB_CODE"));
+                row.setSalLEVEL(rset.getString("SAL_LEVEL"));
+                row.setSalary(rset.getInt("SALARY"));
+                row.setBonus(rset.getDouble("BONUS"));
+                row.setManagerID(rset.getString("MANAGER_ID"));
+                row.setHireDATE(rset.getDate("HIRE_DATE"));
+                row.setEntDATE(rset.getDate("ENT_DATE"));
+                row.setManagerID(rset.getString("ENT_YN"));
+
+                empList.add(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (InvalidPropertiesFormatException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(rset);
+            close(pstmt);
+            close(con);
+        }
+        for (EmployeeDTO emp : empList) {
+            System.out.println(emp);
+        }
+    }
+}

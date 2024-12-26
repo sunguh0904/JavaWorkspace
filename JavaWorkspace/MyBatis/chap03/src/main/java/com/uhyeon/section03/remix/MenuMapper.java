5d27836c14b3fe2caaa5b70be2e85fe7978fc4ad
sunguh0904 <sunguh0904@gmail.com>
2024-12-26 20:01:24 +0900

    chap03 section03 remix

---
+++ JavaWorkspace/MyBatis/chap03/src/main/java/com/uhyeon/section03/remix/MenuMapper.java
@@ -0,0 +1,15 @@
+package com.uhyeon.section03.remix;
+
+import java.util.List;
+
+public interface MenuMapper {
+    List<MenuDTO> selectAllMenu();
+
+    MenuDTO selectMenuByCode(int code);
+
+    int insertMenu(MenuDTO menu);
+
+    int updateMenu(MenuDTO menu);
+
+    int deleteMenu(int code);
+}
